d4b283cf881c29ab4a3883813ffdba92298b2682
Udayakumar, Anandhu <audayakumar@deloitte.com>
2023-12-26 13:24:52 +0530

    inital setup

---
+++ todo-v1/src/main/java/com/todo/todov1/TodoV1Application.java
@@ -0,0 +1,13 @@
+package com.todo.todov1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoV1Application.class, args);
+	}
+
+}
